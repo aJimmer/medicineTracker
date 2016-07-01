1238d39c2f420a61e5c8ebd2f1fd046bce42c545
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2016-06-30 21:27:48 -0700

    finished logic for TrackerReminderVC

--- medTracker/TrackerRemindVC.swift
+++ medTracker/TrackerRemindVC.swift
@@ -53,53 +53,199 @@ class TrackerRemindVC: UIViewController {
     
     @IBAction func morningSwitchTapped(sender: UISwitch) {
         
+        storedDate = getDate()
         
-        
+        if switchMorning.on {
+            defaults.setObject(storedDate, forKey: "storedDate")
+            defaults.setObject(switchMorning.on, forKey: "takenMorning")
+            switchMorning.setOn(true, animated: true)
+            xMorning.text = "✔︎"
+            xMorning.textColor = UIColor.greenColor()
+        }   else {
+            
+            if let _ = defaults.stringForKey("takenMorning") {
+                xMorning.text = "✘"
+                xMorning.textColor = UIColor.redColor()
+                switchMorning.setOn(false, animated: true)
+                defaults.removeObjectForKey("takenMorning")
+            }
+        }
     }
     
     @IBAction func afternoonSwitchTapped(sender: UISwitch) {
         
+        storedDate = getDate()
         
+        if switchAfternoon.on {
+            defaults.setObject(storedDate, forKey: "storedDate")
+            defaults.setObject(switchAfternoon.on, forKey: "takenAfternoon")
+            switchAfternoon.setOn(true, animated: true)
+            xAfternoon.text = "✔︎"
+            xAfternoon.textColor = UIColor.greenColor()
+        }   else {
+            
+            if let _ = defaults.stringForKey("takenAfternoon") {
+                xAfternoon.text = "✘"
+                xAfternoon.textColor = UIColor.redColor()
+                switchAfternoon.setOn(false, animated: true)
+                defaults.removeObjectForKey("takenAfternoon")
+            }
+        }
         
     }
     
     @IBAction func eveningSwitchTapped(sender: UISwitch) {
         
+        storedDate = getDate()
         
+        if switchEvening.on {
+            defaults.setObject(storedDate, forKey: "storedDate")
+            defaults.setObject(switchEvening.on, forKey: "takenEvening")
+            switchEvening.setOn(true, animated: true)
+            xEvening.text = "✔︎"
+            xEvening.textColor = UIColor.greenColor()
+        }   else {
+            
+            if let _ = defaults.stringForKey("takenEvening") {
+                xEvening.text = "✘"
+                xEvening.textColor = UIColor.redColor()
+                switchEvening.setOn(false, animated: true)
+                defaults.removeObjectForKey("takenEvening")
+            }
+        }
         
     }
     
     @IBAction func bedtimeSwitchTapped(sender: UISwitch) {
         
+        storedDate = getDate()
         
+        if switchBedtime.on {
+            defaults.setObject(storedDate, forKey: "storedDate")
+            defaults.setObject(switchBedtime.on, forKey: "takenBedtime")
+            switchBedtime.setOn(true, animated: true)
+            xBedtime.text = "✔︎"
+            xBedtime.textColor = UIColor.greenColor()
+        }   else {
+            
+            if let _ = defaults.stringForKey("takenBedtime") {
+                xBedtime.text = "✘"
+                xBedtime.textColor = UIColor.redColor()
+                switchBedtime.setOn(false, animated: true)
+                defaults.removeObjectForKey("takenBedtime")
+            }
+        }
         
     }
     
     var morningNotification = UILocalNotification()
     @IBAction func morningReminderTapped(sender: UISwitch) {
         
-        
-        
+        if switchReminderMorning.on {
+            defaults.setObject(switchReminderMorning.on, forKey: "reminderMorning")
+            
+            let theDate = NSDate()
+            let calendar = NSCalendar.currentCalendar()
+            calendar.timeZone = NSCalendar.currentCalendar().timeZone
+            let fireDate = calendar.dateBySettingHour(9, minute: 0, second: 0, ofDate: theDate, options: NSCalendarOptions())
+            
+            morningNotification.alertBody = "Did you take your morning pills today?"
+            morningNotification.repeatInterval = NSCalendarUnit.Day
+            morningNotification.fireDate = fireDate
+            morningNotification.soundName = UILocalNotificationDefaultSoundName
+            
+            UIApplication.sharedApplication().scheduleLocalNotification(morningNotification)
+            xReminderMorning.text = "✔︎"
+            xReminderMorning.textColor = UIColor.greenColor()
+        } else {
+            defaults.removeObjectForKey("reminderMorning")
+            UIApplication.sharedApplication().cancelLocalNotification(morningNotification)
+            xReminderMorning.text = "✘"
+            xReminderMorning.textColor = UIColor.redColor()
+        }
     }
     
     var afternoonNotification = UILocalNotification()
     @IBAction func afternoonReminderTapped(sender: UISwitch) {
         
-        
+        if switchReminderAfternoon.on {
+            defaults.setObject(switchReminderAfternoon.on, forKey: "reminderAfternoon")
+            
+            let theDate = NSDate()
+            let calendar = NSCalendar.currentCalendar()
+            calendar.timeZone = NSCalendar.currentCalendar().timeZone
+            let fireDate = calendar.dateBySettingHour(14, minute: 0, second: 0, ofDate: theDate, options: NSCalendarOptions())
+            
+            afternoonNotification.alertBody = "Did you take your afternoon pills today?"
+            afternoonNotification.repeatInterval = NSCalendarUnit.Day
+            afternoonNotification.fireDate = fireDate
+            afternoonNotification.soundName = UILocalNotificationDefaultSoundName
+            
+            UIApplication.sharedApplication().scheduleLocalNotification(afternoonNotification)
+            xReminderAfternoon.text = "✔︎"
+            xReminderAfternoon.textColor = UIColor.greenColor()
+        } else {
+            defaults.removeObjectForKey("reminderAfternoon")
+            UIApplication.sharedApplication().cancelLocalNotification(afternoonNotification)
+            xReminderAfternoon.text = "✘"
+            xReminderAfternoon.textColor = UIColor.redColor()
+        }
         
     }
     
     var eveningNotification = UILocalNotification()
     @IBAction func eveningReminderTapped(sender: UISwitch) {
         
-        
+        if switchReminderEvening.on {
+            defaults.setObject(switchReminderEvening.on, forKey: "reminderEvening")
+            
+            let theDate = NSDate()
+            let calendar = NSCalendar.currentCalendar()
+            calendar.timeZone = NSCalendar.currentCalendar().timeZone
+            let fireDate = calendar.dateBySettingHour(14, minute: 0, second: 0, ofDate: theDate, options: NSCalendarOptions())
+            
+            eveningNotification.alertBody = "Did you take your evening pills today?"
+            eveningNotification.repeatInterval = NSCalendarUnit.Day
+            eveningNotification.fireDate = fireDate
+            eveningNotification.soundName = UILocalNotificationDefaultSoundName
+            
+            UIApplication.sharedApplication().scheduleLocalNotification(eveningNotification)
+            xReminderEvening.text = "✔︎"
+            xReminderEvening.textColor = UIColor.greenColor()
+        } else {
+            defaults.removeObjectForKey("reminderEvening")
+            UIApplication.sharedApplication().cancelLocalNotification(eveningNotification)
+            xReminderEvening.text = "✘"
+            xReminderEvening.textColor = UIColor.redColor()
+        }
         
     }
     
     var bedtimeNotification = UILocalNotification()
     @IBAction func bedtimeReminderTapped(sender: UISwitch) {
         
-        
+        if switchReminderBedtime.on {
+            defaults.setObject(switchReminderBedtime.on, forKey: "reminderBedtime")
+            
+            let theDate = NSDate()
+            let calendar = NSCalendar.currentCalendar()
+            calendar.timeZone = NSCalendar.currentCalendar().timeZone
+            let fireDate = calendar.dateBySettingHour(14, minute: 0, second: 0, ofDate: theDate, options: NSCalendarOptions())
+            
+            bedtimeNotification.alertBody = "Did you take your bedtime pills today?"
+            bedtimeNotification.repeatInterval = NSCalendarUnit.Day
+            bedtimeNotification.fireDate = fireDate
+            bedtimeNotification.soundName = UILocalNotificationDefaultSoundName
+            
+            UIApplication.sharedApplication().scheduleLocalNotification(bedtimeNotification)
+            xReminderBedtime.text = "✔︎"
+            xReminderBedtime.textColor = UIColor.greenColor()
+        } else {
+            defaults.removeObjectForKey("reminderBedtime")
+            UIApplication.sharedApplication().cancelLocalNotification(bedtimeNotification)
+            xReminderBedtime.text = "✘"
+            xReminderBedtime.textColor = UIColor.redColor()
+        }
         
     }
   
